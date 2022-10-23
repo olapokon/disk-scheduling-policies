bea1fd9f46985e365807297fd48c9588cacb2eba
olapokon <aryjna1@gmail.com>
2022-10-23 19:29:37 +0300

    Refactor to receive command line arguments

--- DiskSchedulingPolicies.java
+++ DiskSchedulingPolicies.java
@@ -35,7 +35,6 @@ public class DiskSchedulingPolicies {
             final int loc1 = locations.get(i - 1);
             final int loc2 = locations.get(i);
             final int distance = Math.abs(loc1 - loc2);
-            // System.out.printf("Loc 1: %s, Loc 2: %s, distance: %d\n", loc1, loc2, distance);
             distancesSum += distance;
         }
         System.out.printf("Sum of distances: %d\n", distancesSum);
@@ -44,19 +43,15 @@ public class DiskSchedulingPolicies {
     }
 
     private static List<Integer> fifoOrder(List<Integer> requests) {
-        System.out.println("FIFO policy");
-        System.out.printf("Starting location: %d\n", requests.get(0));
         System.out.printf("FIFO order: %s\n", requests);
         return requests;
     }
 
     private static List<Integer> sstfOrder(List<Integer> requests) {
-        System.out.println("SSTF policy");
         final int startingLocation = requests.get(0);
         final List<Integer> sortedLocations = requests.stream()
                 .sorted()
                 .toList();
-        System.out.printf("Starting location: %d\n", startingLocation);
 
         final List<Integer> sstfLocations = new ArrayList<>();
         sstfLocations.add(startingLocation);
@@ -86,8 +81,13 @@ public class DiskSchedulingPolicies {
         return sstfLocations;
     }
 
+    /**
+     * Following the course material, SCAN goes all the way to the last track and all the way to the first track.
+     *
+     * Because of this, the result is not exactly the same as in Stallings,
+     * where it stops at the highest and lowest location requested.
+     */
     private static List<Integer> scanOrder(List<Integer> requests, Integer numberOfTracks) {
-        System.out.println("SCAN policy");
         if (numberOfTracks == null) {
             throw new IllegalArgumentException();
         }
@@ -96,8 +96,6 @@ public class DiskSchedulingPolicies {
                 .sorted()
                 .toList();
         int startingLocationIdx = Collections.binarySearch(sortedLocations, startingLocation);
-        System.out.printf("Starting location: %d\n", startingLocation);
-        System.out.printf("Number of tracks: %d\n", numberOfTracks);
 
         final List<Integer> scanLocations = new ArrayList<>();
         // add all locations starting from the starting location to the highest requested location
@@ -115,16 +113,18 @@ public class DiskSchedulingPolicies {
         return scanLocations;
     }
 
-    public static void averageTracksTraversed(String requests, Policy policy) {
-        averageTracksTraversed(requests, policy, null);
+    public static void averageTracksTraversed(String requests, int startingLocation, Policy policy) {
+        averageTracksTraversed(requests, startingLocation, policy, null);
     }
 
-    public static void averageTracksTraversed(String requests, Policy policy, Integer numberOfTracks) {
-        List<Integer> requestsList = Arrays.stream(requests.split(" "))
+    public static void averageTracksTraversed(String requests, int startingLocation, Policy policy,
+            Integer numberOfTracks) {
+        List<Integer> requestsList = Stream.concat(Stream.of(String.valueOf(startingLocation)),
+                Arrays.stream(requests.split(" ")))
                 .filter(s -> !s.isBlank())
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        int locationsRequestedCount = requestsList.size() - 1; // do not count beginning location
+        int locationsRequestedCount = requestsList.size() - 1; // do not count starting location
         switch (policy) {
             case FIFO -> averageOfDistances(fifoOrder(requestsList), locationsRequestedCount);
             case SSTF -> averageOfDistances(sstfOrder(requestsList), locationsRequestedCount);
@@ -133,16 +133,42 @@ public class DiskSchedulingPolicies {
         }
     }
 
+    public static void allPolicies(String locationsRequested, int startingLocation, int numberOfTracks) {
+        System.out.printf("""
+                Locations requested: %s
+                Starting location: %s
+                Number of tracks: %s\n
+                """,
+                locationsRequested, startingLocation, numberOfTracks);
+        averageTracksTraversed(locationsRequested, startingLocation, Policy.FIFO);
+        System.out.println();
+        averageTracksTraversed(locationsRequested, startingLocation, Policy.SSTF);
+        System.out.println();
+        averageTracksTraversed(locationsRequested, startingLocation, Policy.SCAN, numberOfTracks);
+        System.out.println();
+    }
+
     public static void main(String[] args) {
-        final String locationsRequested = "100 55 58 39 18 90 160 150 38 184";
-        final int numberOfTracks = 200;
-        System.out.printf("Locations requested, including starting location 100: %s\nNumber of tracks: %s\n\n",
-                locationsRequested, numberOfTracks);
-        averageTracksTraversed(locationsRequested, Policy.FIFO);
-        System.out.println();
-        averageTracksTraversed(locationsRequested, Policy.SSTF);
-        System.out.println();
-        averageTracksTraversed(locationsRequested, Policy.SCAN, numberOfTracks);
-        System.out.println();
+        // System.out.println(Arrays.toString(args));
+        switch (args.length) {
+            case 0 -> {
+                System.out.println("No arguments passed, using default values.");
+                final String locationsRequested = "55 58 39 18 90 160 150 38 184";
+                final int startingLocation = 100;
+                final int numberOfTracks = 200;
+                allPolicies(locationsRequested, startingLocation, numberOfTracks);
+            }
+            case 3 -> {
+                final String locationsRequested = args[0];
+                final int startingLocation = Integer.parseInt(args[1]);
+                final int numberOfTracks = Integer.parseInt(args[2]);
+                allPolicies(locationsRequested, startingLocation, numberOfTracks);
+            }
+            default -> {
+                System.err.println("Invalid arguments. "
+                        + "Usage: disk-scheduling-policies <requests> <starting location> <number of tracks>");
+                System.exit(1);
+            }
+        }
     }
 }
