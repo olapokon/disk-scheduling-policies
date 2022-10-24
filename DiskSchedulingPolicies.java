9fa7f12065b842f93e64370d10d1db7fa9668e8e
olapokon <aryjna1@gmail.com>
2022-10-24 17:30:06 +0300

    Add C-SCAN, LOOK, and C-LOOK policies

--- DiskSchedulingPolicies.java
+++ DiskSchedulingPolicies.java
@@ -9,12 +9,22 @@ public class DiskSchedulingPolicies {
         FIFO,
 
         /**
-         * Shortest Seek Time First
+         * Shortest Service Time First
          */
         SSTF,
 
         SCAN,
+
+        CSCAN,
+
+        LOOK,
+
+        CLOOK,
     }
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_PURPLE = "\u001B[35m";
 
     /**
      * @param locations
@@ -40,12 +50,13 @@ public class DiskSchedulingPolicies {
         }
         System.out.printf("Total track traversals: %d\n", distancesSum);
         final double averageTracksTraversed = (double) distancesSum / locationsRequestedCount;
-        System.out.printf("Average tracks traversed per request: %f\n", averageTracksTraversed);
+        System.out.printf("Average tracks traversed per request: %s%f%s\n",
+                          ANSI_PURPLE, averageTracksTraversed, ANSI_RESET);
     }
 
     private static List<Integer> fifoOrder(List<Integer> requests)
     {
-        System.out.printf("FIFO order: %s\n", requests);
+        printPolicyOrder(Policy.FIFO, requests);
         return requests;
     }
 
@@ -80,47 +91,89 @@ public class DiskSchedulingPolicies {
             }
         }
 
-        System.out.printf("SSTF order: %s\n", sstfLocations);
+        printPolicyOrder(Policy.SSTF, sstfLocations);
         return sstfLocations;
     }
 
     /**
-     * SCAN goes all the way to the last track and all the way to the first track.
-     *
-     * In the "Comparison of Disk Scheduling Algorithms" table in Stallings,
-     * the SCAN column is actually the LOOK policy, an optimization on SCAN, so the result is different.
+     * Returns the order for both SCAN and LOOK policies.
      */
-    private static List<Integer> scanOrder(List<Integer> requests, Integer numberOfTracks)
+    private static List<Integer> scanOrder(List<Integer> requests,
+                                           Integer numberOfTracks,
+                                           boolean lookOptimization)
     {
         if (numberOfTracks == null) {
             throw new IllegalArgumentException();
         }
         final int startingLocation = requests.get(0);
-        final List<Integer> sortedLocations = requests.stream()
-                .sorted()
-                .toList();
+        final int lastTrack = numberOfTracks - 1;
+        final List<Integer> sortedLocations = requests.stream().sorted().toList();
         final int startingLocationIdx = Collections.binarySearch(sortedLocations, startingLocation);
 
         final List<Integer> scanLocations = new ArrayList<>();
         // add all locations starting from the starting location to the highest requested location
         scanLocations.addAll(sortedLocations.subList(startingLocationIdx, sortedLocations.size()));
-        if (scanLocations.get(scanLocations.size() - 1) != numberOfTracks - 1) {
-            // add the last track
+        if (!lookOptimization
+                && scanLocations.get(scanLocations.size() - 1) != lastTrack) {
             // if the highest requested location is not the last track
-            scanLocations.add(numberOfTracks - 1);
+            scanLocations.add(lastTrack);
         }
-        // add all locations from the starting locations to the lowest requested location
+        // add all locations from the starting location to the lowest requested location
         for (int i = startingLocationIdx - 1; i >= 0; --i) {
             scanLocations.add(sortedLocations.get(i));
         }
-        if (scanLocations.get(scanLocations.size() - 1) != 0) {
-            // add the first track
+        if (!lookOptimization && scanLocations.get(scanLocations.size() - 1) != 0) {
             // if the lowest requested location is not the first track
             scanLocations.add(0);
         }
 
-        System.out.printf("SCAN order: %s\n", scanLocations);
+        printPolicyOrder(lookOptimization ? Policy.LOOK : Policy.SCAN, scanLocations);
         return scanLocations;
+    }
+
+    /**
+     * Returns the order for both C-SCAN and C-LOOK policies.
+     */
+    private static List<Integer> cScanOrder(List<Integer> requests,
+                                            Integer numberOfTracks,
+                                            boolean lookOptimization)
+    {
+        if (numberOfTracks == null) {
+            throw new IllegalArgumentException();
+        }
+        final int startingLocation = requests.get(0);
+        final int lastTrack = numberOfTracks - 1;
+        final List<Integer> sortedLocations = requests.stream().sorted().toList();
+        final int startingLocationIdx = Collections.binarySearch(sortedLocations, startingLocation);
+
+        final List<Integer> scanLocations = new ArrayList<>();
+        // add all locations starting from the starting location to the highest requested location
+        scanLocations.addAll(sortedLocations.subList(startingLocationIdx, sortedLocations.size()));
+        if (!lookOptimization
+                && scanLocations.get(scanLocations.size() - 1) != lastTrack) {
+            // if the highest requested location is not the last track
+            scanLocations.add(lastTrack);
+        }
+        if (!lookOptimization && sortedLocations.get(0) != 0) {
+            // if the lowest requested location is not the first track
+            scanLocations.add(0);
+        }
+        // add all locations from the lowest requested location to the starting location
+        scanLocations.addAll(sortedLocations.subList(0, startingLocationIdx));
+
+        printPolicyOrder(lookOptimization ? Policy.CLOOK : Policy.CSCAN, scanLocations);
+        return scanLocations;
+    }
+
+    private static void printPolicyOrder(Policy policy, List<Integer> order)
+    {
+        String policyName = switch (policy) {
+            case CSCAN -> "C-SCAN";
+            case CLOOK -> "C-LOOK";
+            default -> policy.name();
+        };
+        String policyText = ANSI_GREEN + policyName + ANSI_RESET;
+        System.out.printf("%s order: %s\n", policyText, order);
     }
 
     public static void averageTracksTraversed(String requests, int startingLocation, Policy policy)
@@ -143,8 +196,14 @@ public class DiskSchedulingPolicies {
         switch (policy) {
             case FIFO -> averageOfDistances(fifoOrder(requestsList), locationsRequestedCount);
             case SSTF -> averageOfDistances(sstfOrder(requestsList), locationsRequestedCount);
-            case SCAN -> averageOfDistances(scanOrder(requestsList, numberOfTracks),
+            case SCAN -> averageOfDistances(scanOrder(requestsList, numberOfTracks, false),
                                             locationsRequestedCount);
+            case CSCAN -> averageOfDistances(cScanOrder(requestsList, numberOfTracks, false),
+                                             locationsRequestedCount);
+            case LOOK -> averageOfDistances(scanOrder(requestsList, numberOfTracks, true),
+                                            locationsRequestedCount);
+            case CLOOK -> averageOfDistances(cScanOrder(requestsList, numberOfTracks, true),
+                                             locationsRequestedCount);
             default -> throw new IllegalArgumentException();
         }
     }
@@ -163,6 +222,12 @@ public class DiskSchedulingPolicies {
         averageTracksTraversed(locationsRequested, startingLocation, Policy.SSTF);
         System.out.println();
         averageTracksTraversed(locationsRequested, startingLocation, Policy.SCAN, numberOfTracks);
+        System.out.println();
+        averageTracksTraversed(locationsRequested, startingLocation, Policy.CSCAN, numberOfTracks);
+        System.out.println();
+        averageTracksTraversed(locationsRequested, startingLocation, Policy.LOOK, numberOfTracks);
+        System.out.println();
+        averageTracksTraversed(locationsRequested, startingLocation, Policy.CLOOK, numberOfTracks);
         System.out.println();
     }
 
